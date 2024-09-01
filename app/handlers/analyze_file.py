fc295b5558a030e1249f6605eb9f88b5687a0698
Sid Narayanan <what-the-sid@Sids-MacBook-Pro.local>
2024-09-01 08:20:35 +0530

    chore - Removed print statements

--- app/handlers/analyze_file.py
+++ app/handlers/analyze_file.py
@@ -6,8 +6,7 @@ from app.schema import QAResponse
 async def handler(questions_file: UploadFile, chain) -> QAResponse:
     results = []
     questions = await parse_json_file(questions_file)
-
-    print(questions)
+    
     for question in questions:
         try:
             response = chain.invoke({"input": question })
